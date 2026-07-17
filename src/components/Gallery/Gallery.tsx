54d1a9ce18e53888afba3c95dab2d2018493d9a5
xamproctor <xamproctor@gmail.com>
2026-07-17 15:14:11 +0530

    remove photos

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,13 +1,11 @@
 import { motion } from "framer-motion";
 import image1 from "../../assets/images/image1.png";
-import image2 from "../../assets/images/image2.png";
-import image3 from "../../assets/images/image3.png";
+
 import image4 from "../../assets/images/image4.png";
 
 const images = [
   image1,
-  image2,
-  image3,
+  
   image4,
 ];
 
