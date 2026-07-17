b43299ef15c31ee5f3d5b28168312c4c0907e401
xamproctor <xamproctor@gmail.com>
2026-07-17 14:08:10 +0530

    Describe your changes

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
-import image from "../../assets/images/image3.png";
 import image1 from "../../assets/images/image1.png";
 import image2 from "../../assets/images/image2.png";
+import image3 from "../../assets/images/image3.png";
 import image4 from "../../assets/images/image4.png";
 
 const images = [
-  image,
   image1,
   image2,
+  image3,
   image4,
 ];
 
